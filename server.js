c9beafd52626ed9c71761c3c68e9e6ce44d8cbd2
Mike Adossi <mikeadossi@gmail.com>
2020-08-20 23:32:33 -0700

    scrape more page elements

--- server.js
+++ server.js
@@ -16,11 +16,20 @@ let scrape = async () => {
         const page = await browser.newPage();
         page.setViewport({ width: 1920, height: 1080 });
 
+        const pageElements = [
+            'div[aria-label="Standard Range Plus"]',
+            'div[aria-label="Long Range"]',
+            'div[aria-label="Performance"]'
+        ]; 
+
         await page.goto('https://www.tesla.com/model3/design#battery', { timeout: 60 * 1000 }); 
-        await page.waitForSelector('div[aria-label="Long Range"]');
+        await page.waitForSelector('div[aria-label="Performance"]');
 
         const result = await page.evaluate(() => {
-            let prices = document.querySelector('div[aria-label="Long Range"]').innerText;
+            // let prices = document.querySelector(pageElements.model3SR).innerText;
+            // let prices = document.querySelector(pageElements.model3LR).innerText;
+            let prices = document.querySelector('div[aria-label="Performance"]').innerText;
+            
             let articles = {};
 
             if (prices.length > 0) { 
