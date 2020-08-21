873a143ee514a69d862e53d602a8ef24dd647774
Mike Adossi <mikeadossi@gmail.com>
2020-08-21 16:59:57 -0700

    scrape paint data

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ let scrape = async () => {
         const page = await browser.newPage();
         page.setViewport({ width: 1920, height: 1080 });
 
+        // pageElements isn't being used yet, but will need to be utilized in future
         const pageElements = [
             'div[aria-label="Standard Range Plus"]',
             'div[aria-label="Long Range"]',
@@ -25,37 +26,47 @@ let scrape = async () => {
         ]; 
 
         await page.goto('https://www.tesla.com/model3/design#battery', { timeout: 60 * 1000 }); 
-        await page.waitForSelector('div[aria-label="Performance"]');
 
-        const result = await page.evaluate(() => {  
-            
-            let articles = {};
+        const m3BatteryResults = async () => { 
+            await page.waitForSelector('div[aria-label="Performance"]'); 
+            const result = await page.evaluate(() => {
 
-            let prices = document.querySelector('div[aria-label="Performance"]').innerText;
+                let m3Prices = document.querySelector('div[aria-label="Performance"]').innerText;
+                let m3Specs = document.querySelector('.battery-and-drive-specs--list').innerText;
+                let m3DeliveryDate = document.querySelector('.delivery-timing--date').innerText;
 
-            if (prices.length > 0) { 
-                articles.price = prices
+                return { m3Prices, m3Specs, m3DeliveryDate }
+
+            }); 
+            return result; 
+        };
+
+
+        const m3ExteriorResults = async () => {
+            await page.waitForSelector('.packages-options--nav-item');
+            await page.click('.packages-options--nav-item[arial-label="paint"]');
+
+            // get "puppeteer reference" of the elements (different from the actual DOM reference when using e.g. page.querySelector)
+            const colorButtonsRefs = await page.$$('.child-group--container__PAINT .group--options_asset--container[role="button"]');
+            let result = [];
+            for (let colorBtn of colorButtonsRefs) {
+                
+                await colorBtn.click();
+                const colorPriceResult = await page.evaluate(() => {
+                    const colorText = document.querySelector('.child-group--container__PAINT .child-group--selected_option_details span:first-child').innerText;
+                    const priceText = document.querySelector('.child-group--container__PAINT .child-group--selected_option_details span:nth-child(2)').innerText;
+                    
+                    return { colorText, priceText }; 
+                })
+                
+                result.push(colorPriceResult);
+    
             }
+            return result;
+        }
 
-            let specs = document.querySelector('.battery-and-drive-specs--list').innerText;
+        return [await m3BatteryResults(), await m3ExteriorResults()];
 
-            if (specs.length > 0) { 
-                articles.specs = specs
-            }
-
-            let deliveryDate = document.querySelector('.delivery-timing--date').innerText;
-
-            if (deliveryDate.length > 0) { 
-                articles.deliveryDate = deliveryDate
-            }
-
-            return {
-                articles
-            }
-
-        });
-
-        return result;
 
     } catch (err) {
         console.log(err)
