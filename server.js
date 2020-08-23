b26bee2935c823cfdbe332b0b66974cf8fb06beb
Mike Adossi <mikeadossi@gmail.com>
2020-08-22 22:46:08 -0700

    scrape all car exterior data

--- server.js
+++ server.js
@@ -41,47 +41,51 @@ let scrape = async () => {
             return result; 
         };
 
+        const allExteriorResults = async () => {
+            let webpagesArr = ['https://www.tesla.com/model3/design#battery', 'https://www.tesla.com/models/design#battery', 'https://www.tesla.com/modelx/design#battery', 'https://www.tesla.com/modely/design#battery'];
+            let store = {model3:[], modelS:[], modelX:[], modelY:[]};
+            let model = ['model3', 'modelS', 'modelX', 'modelY'];
 
-        const m3ExteriorResults = async () => {
-            await page.waitForSelector('.packages-options--nav-item');
-            await page.click('.packages-options--nav-item[arial-label="paint"]');
-
-            let result = [];
-
-            // get "puppeteer reference" of the elements (different from the actual DOM reference when using e.g. page.querySelector)
-            const colorButtonsRefs = await page.$$('.child-group--container__PAINT .group--options_asset--container[role="button"]');
-
-            for (let colorBtn of colorButtonsRefs) {
-                
-                await colorBtn.click();
-                const colorPriceResult = await page.evaluate(() => {
-                    const colorText = document.querySelector('.child-group--container__PAINT .child-group--selected_option_details span:first-child').innerText;
-                    const priceText = document.querySelector('.child-group--container__PAINT .child-group--selected_option_details span:nth-child(2)').innerText;
+            for( i in webpagesArr ){
+                await page.goto(webpagesArr[i], { timeout: 60 * 1000 }); 
+                await page.waitForSelector('.packages-options--nav-item');
+                await page.click('.packages-options--nav-item[arial-label="paint"]');
+    
+                const colorButtonsRefs = await page.$$('.child-group--container__PAINT .group--options_asset--container[role="button"]');
+    
+                for (let colorBtn of colorButtonsRefs) {
                     
-                    return { colorText, priceText }; 
-                })
-
-                result.push(colorPriceResult);
-            }
-
-            const wheelRefs = await page.$$('.child-group--container__WHEELS .group--options_asset--container[role="button"]');
-
-            for (let wheelsBtn of wheelRefs) {
-
-                let classNames = wheelsBtn._remoteObject.description
-                
-                // the if statement below removes all hidden button elements (this proved to be a nuisance)
-                if(classNames.indexOf('.group--option--hidden') === -1){
-                    await wheelsBtn.click();
-                    const wheelsResult = await page.evaluate(() => {
-                        const wheelName = document.querySelector('.child-group--container.child-group--container__WHEELS .child-group--selected_option_details span:nth-child(1) span').innerText;
-                        const wheelPrice = document.querySelector('.child-group--container.child-group--container__WHEELS .child-group--selected_option_details span:nth-child(2) span').innerText;
-                        return { wheelName, wheelPrice };
+                    await colorBtn.click();
+                    const colorPriceResult = await page.evaluate(() => {
+                        const colorText = document.querySelector('.child-group--container__PAINT .child-group--selected_option_details span:first-child').innerText;
+                        const priceText = document.querySelector('.child-group--container__PAINT .child-group--selected_option_details span:nth-child(2)').innerText;
+                        
+                        return { colorText, priceText }; 
                     })
-                    result.push(wheelsResult);
-                } 
+    
+                    store[model[i]].push(colorPriceResult);
+                }
+    
+                const wheelRefs = await page.$$('.child-group--container__WHEELS .group--options_asset--container[role="button"]');
+    
+                for (let wheelsBtn of wheelRefs) {
+    
+                    let classNames = wheelsBtn._remoteObject.description
+                    
+                    // the if statement below removes all hidden button elements (this proved to be a nuisance)
+                    if(classNames.indexOf('.group--option--hidden') === -1){
+                        await wheelsBtn.click();
+                        const wheelsResult = await page.evaluate(() => {
+                            const wheelName = document.querySelector('.child-group--container.child-group--container__WHEELS .child-group--selected_option_details span:nth-child(1) span').innerText;
+                            const wheelPrice = document.querySelector('.child-group--container.child-group--container__WHEELS .child-group--selected_option_details span:nth-child(2) span').innerText;
+                            return { wheelName, wheelPrice };
+                        })
+                        store[model[i]].push(wheelsResult);
+                    } 
+                }
+ 
             }
-            return result;
+            return store;
         }
 
         const m3InteriorResults = async () => {
@@ -119,7 +123,23 @@ let scrape = async () => {
             return fsdPrice;
         }
 
-        return [ await m3BatteryResults(), await m3ExteriorResults(), await m3InteriorResults(), await m3FSD()];
+        const mSBatteryResults = async () => {
+            await page.goto('https://www.tesla.com/models/design#battery', { timeout: 60 * 1000 }); 
+            await page.waitForSelector('div[aria-label="Long Range Plus"]'); 
+            const result = await page.evaluate(() => {
+
+                let mSPrices = document.querySelector('div[aria-label="Long Range Plus"]').innerText;
+                let mSSpecs = document.querySelector('.battery-and-drive-specs--list').innerText;
+                let mSDeliveryDate = document.querySelector('.delivery-timing--date').innerText;
+
+                return { mSPrices, mSSpecs, mSDeliveryDate }
+
+            }); 
+            return result; 
+        };
+
+
+        return [ await m3BatteryResults(), await allExteriorResults(), await m3InteriorResults(), await m3FSD(), await mSBatteryResults() ];
 
 
     } catch (err) {
