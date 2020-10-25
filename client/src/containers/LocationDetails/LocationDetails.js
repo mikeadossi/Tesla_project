158030af300780b150af0641398780e08887de3a
Mike Adossi <mikeadossi@gmail.com>
2020-10-25 10:38:20 -0500

    create LocationDetails UI

--- client/src/containers/LocationDetails/LocationDetails.js
+++ client/src/containers/LocationDetails/LocationDetails.js
@@ -5,7 +5,31 @@ class LocationDetails extends Component {
   render() {
     return ( 
       <div className="locationDetails_container">
-          Location Details
+        <div className="locationDetails_container locationDetails_container_1">
+          <div className="LocationDetails_zipcode LocationDetails_element">90210</div>
+          <div className="LocationDetails_city LocationDetails_element">Beverly Hills, CA</div>
+          <div className="LocationDetails_county LocationDetails_element">Alameda county</div>
+        </div> 
+        <div className="locationDetails_container locationDetails_container_2">
+          <div className="locationDetails_areacode">(213)</div>
+          <div className="locationDetails_areacode_subtext">area code</div> 
+        </div>
+        <div className="locationDetails_container locationDetails_container_3">
+          <div className="locationDetails_time_digital">2:39 AM</div>
+          <div className="locationDetails_timezone">Pacific Time</div>
+        </div>
+        <div className="locationDetails_container locationDetails_container_4">
+          <div className="locationDetails_day">Monday</div>
+          <div className="locationDetails_date">Oct 26, 2020</div>
+        </div>
+        <div className="locationDetails_container locationDetails_container_4">
+          <div className="locationDetails_region_name">BAY AREA</div>
+          <div className="locationDetails_region_tag">Region</div>
+        </div>
+        <div className="locationDetails_container locationDetails_container_5">
+          <div className="locationDetails_project_sun">proj. sun</div>
+          <div className="locationDetails_weather">weather</div>
+        </div>
       </div>  
     );
   }
--- client/src/containers/ProductMenu/ProductMenu.js
+++ client/src/containers/ProductMenu/ProductMenu.js
@@ -5,7 +5,7 @@ class ProductMenu extends Component {
   render() {
     return (
       <div className="productMenu_container">
-        <div className="prductMenu_btn_container">
+        <div className="productMenu_btn_container">
             <div className="productMenu_btn vehicle_menu_btn">Vehicle</div>
             <div className="productMenu_btn solar_menu_btn">Solar</div>
         </div>
