d24f1da621c791801b51f87ce1a5f89c812a38f5
Mike Adossi <mikeadossi@gmail.com>
2020-11-29 23:40:21 -0600

    create user entry Vehicle forms

--- client/src/components/VehicleData/VehicleConfig/VehicleConfig.js
+++ client/src/components/VehicleData/VehicleConfig/VehicleConfig.js
@@ -3,6 +3,9 @@ import './VehicleConfig.css';
 import VehicleCash from '../../VehicleData/Vehicle_pymt/VehicleCash/VehicleCash';
 import VehicleLeasing from '../../VehicleData/Vehicle_pymt/VehicleLeasing/VehicleLeasing';
 import VehicleFinance from '../../VehicleData/Vehicle_pymt/VehicleFinance/VehicleFinance';
+import Vehicle_userEntry_financing from '../../VehicleData/Vehicle_userEntry/Vehicle_userEntry_financing/Vehicle_userEntry_financing';
+import Vehicle_userEntry_leasing from '../../VehicleData/Vehicle_userEntry/Vehicle_userEntry_leasing/Vehicle_userEntry_leasing';
+import Vehicle_userEntry_cash from '../../VehicleData/Vehicle_userEntry/Vehicle_userEntry_cash/Vehicle_userEntry_cash';
 
 const VehicleConfig = ({selectedVehicle}) => {
 
@@ -175,86 +178,14 @@ const VehicleConfig = ({selectedVehicle}) => {
             <div className="veicleConfig_userEntry_container">
               <div className="veicleConfig_userEntry_subcontainer">
                 <div className="app_displayFlex app_Solar_selectPymt_div">
-                    <div className="app_Solar_selectPymt_btn">Cash</div>
-                    <div className="app_Solar_selectPymt_btn">Loan</div>
-                    <div className="app_Solar_selectPymt_btn">Lease</div>
+                    <div onClick={() => showComponent('Cash')} className="app_Solar_selectPymt_btn">Cash</div>
+                    <div onClick={() => showComponent('Loan')} className="app_Solar_selectPymt_btn">Loan</div>
+                    <div onClick={() => showComponent('Lease')} className="app_Solar_selectPymt_btn">Lease</div>
                 </div>
                 <div className="vehicleConfig_userEntry_containers">
-                  <div className="vehicleConfig_userEntry_container1">
-                    <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_stateTax">
-                      <label>APR: </label>
-                      <span className="vehicleConfig_userInput_container">
-                        <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
-                        <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
-                      </span>
-                    </div>
-                    <div className="vehicleConfig_pricing vehicleConfig_stateFees">
-                      <label>Leasing: </label> 
-                      <select className="vehicleConfig_userSelect vehicleConfig_userSelect_lease">
-                        <option value="24 months">24 months</option>
-                        <option value="36 months">36 months</option>
-                      </select>
-                      <select className="vehicleConfig_userSelect vehicleConfig_userSelect_miles">
-                        <option value="10000 miles">10,000 miles</option>
-                        <option value="15000 miles">12,000 miles</option>
-                        <option value="20000 miles">15,000 miles</option> 
-                      </select>
-                    </div>
-                    <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_cashAmtDue"> 
-                      <label>Finance Term: </label>
-                      <select className="vehicleConfig_userSelect vehicleConfig_userSelect_miles">
-                        <option value="36 months">36 months</option>
-                        <option value="48 months">48 months</option>
-                        <option value="60 months">60 months</option>
-                        <option value="72 months">72 months</option>
-                      </select>
-                    </div>
-                    <div className="vehicleConfig_pricing vehicleConfig_loanAmtDue">
-                      <label>Trade-in Lien Type: </label>
-                      <span className="vehicleConfig_userInput_container">
-                        <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
-                        <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
-                      </span>
-                    </div>
-                    <div className="vehicleConfig_pricing vehicleConfig_customerCashDown">
-                      <label>Cash Down pymt: </label>
-                      <span className="vehicleConfig_userInput_container">
-                        <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
-                        <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="vehicleConfig_userEntry_container2">
-                    <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_stateTax">
-                      <label>Enter Adjustments: </label>
-                      <span className="vehicleConfig_userInput_container">
-                        <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
-                        <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
-                      </span>
-                    </div>
-                    <div className="vehicleConfig_pricing vehicleConfig_stateFees">
-                      <label>Enter trade-in value: </label>
-                      <span className="vehicleConfig_userInput_container">
-                        <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
-                        <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
-                      </span>
-                    </div>
-                    <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_cashAmtDue">
-                      <label>Enter trade-in payoff: </label>
-                      <span className="vehicleConfig_userInput_container">
-                        <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
-                        <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
-                      </span>
-                    </div>
-                    <div className="vehicleConfig_pricing vehicleConfig_loanAmtDue">
-                      <label>Trade-in Equity: </label>
-                      <span className="vehicleConfig_userInput_container">
-                        <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
-                        <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
-                      </span>
-                    </div>
-                  </div>
+                  {visibility.Cash ? <Vehicle_userEntry_cash />:""}
+                  {visibility.Loan ? <Vehicle_userEntry_financing />:""}
+                  {visibility.Lease ? <Vehicle_userEntry_leasing />:""}
                 </div>
               </div>
               <div className="vehicleConfig_submit_btn_container">
---
+++ client/src/components/VehicleData/Vehicle_userEntry/Vehicle_userEntry_leasing/Vehicle_userEntry_leasing.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './Vehicle_userEntry_leasing.css';
+
+const VehicleUserEntry_leasing = () => {
+
+  return (
+    <div className="vehicleUserEntry_leasing_container">
+        <div className="vehicleConfig_userEntry_container1">
+            <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_stateTax">
+                <label>LEASE APR: </label>
+                <span className="vehicleConfig_userInput_container">
+                <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
+                <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
+                </span>
+            </div>
+            <div className="vehicleConfig_pricing vehicleConfig_stateFees">
+                <label>Leasing: </label> 
+                <select className="vehicleConfig_userSelect vehicleConfig_userSelect_lease">
+                <option value="24 months">24 months</option>
+                <option value="36 months">36 months</option>
+                </select>
+                <select className="vehicleConfig_userSelect vehicleConfig_userSelect_miles">
+                <option value="10000 miles">10,000 miles</option>
+                <option value="15000 miles">12,000 miles</option>
+                <option value="20000 miles">15,000 miles</option> 
+                </select>
+            </div>
+            <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_cashAmtDue"> 
+                <label>Finance Term: </label>
+                <select className="vehicleConfig_userSelect vehicleConfig_userSelect_miles">
+                <option value="36 months">36 months</option>
+                <option value="48 months">48 months</option>
+                <option value="60 months">60 months</option>
+                <option value="72 months">72 months</option>
+                </select>
+            </div>
+            <div className="vehicleConfig_pricing vehicleConfig_loanAmtDue">
+                <label>Trade-in Lien Type: </label>
+                <span className="vehicleConfig_userInput_container">
+                <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
+                <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
+                </span>
+            </div>
+            <div className="vehicleConfig_pricing vehicleConfig_customerCashDown">
+                <label>Cash Down pymt: </label>
+                <span className="vehicleConfig_userInput_container">
+                <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
+                <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
+                </span>
+            </div>
+        </div>
+
+        <div className="vehicleConfig_userEntry_container2">
+            <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_stateTax">
+                <label>Enter Adjustments: </label>
+                <span className="vehicleConfig_userInput_container">
+                <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
+                <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
+                </span>
+            </div>
+            <div className="vehicleConfig_pricing vehicleConfig_stateFees">
+                <label>Enter trade-in value: </label>
+                <span className="vehicleConfig_userInput_container">
+                <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
+                <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
+                </span>
+            </div>
+            <div className="vehicleConfig_pricing vehicleConfig_pricing_grey vehicleConfig_cashAmtDue">
+                <label>Enter trade-in payoff: </label>
+                <span className="vehicleConfig_userInput_container">
+                <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
+                <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
+                </span>
+            </div>
+            <div className="vehicleConfig_pricing vehicleConfig_loanAmtDue">
+                <label>Trade-in Equity: </label>
+                <span className="vehicleConfig_userInput_container">
+                <div className="vehicleConfig_userInput_subcontainer">$<input className="vehicleConfig_userInput app_removeBlue"></input></div>
+                <img className="vehicleConfig_edit_btn app_cursorPointer vehicleConfig_userEntry_1" src="../../../../images/edit_icon.png" />
+                </span>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default VehicleUserEntry_leasing;
