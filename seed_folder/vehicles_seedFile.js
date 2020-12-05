9c1056a6e5ec134fb032a5ea7bfe61bf58956255
Mike Adossi <mikeadossi@gmail.com>
2020-12-05 00:16:39 -0600

    remove unnecessary quotes

---
+++ seed_folder/vehicles_seedFile.js
@@ -0,0 +1,501 @@
+module.exports = {
+  model3: {
+    partial_premium_interior:
+      "12-way power adjustable heated front seats <br/> Premium seat material and trim <br/> Upgraded audio – immersive sound <br/> Premium Connectivity (30 days included) <br/> Tinted glass roof with ultraviolet and infrared protection <br/> Power folding, heated side mirrors <br/> Music and media over Bluetooth® <br/> Custom driver profiles <br/> Center console with storage, 4 USB ports and wireless charging for 2 smartphones",
+
+    premium_interior:
+      "12-way power adjustable front seats <br/> Heated front and rear seats <br/> Premium audio – 14 speakers, 1 subwoofer, 2 amps and immersive sound <br/> Premium Connectivity (1 year included) <br/> LED fog lamps <br/> Interior floor mats <br/> Tinted glass roof with ultraviolet and infrared protection <br/> Power folding, heated side mirrors <br/> Music and media over Bluetooth® <br/> Custom driver profiles <br/> Center console with storage, 4 USB ports and wireless charging for 2 smartphones",
+
+    model: "Model 3",
+
+    standard: {
+      purchase_price: 37990,
+      image_vehicle: "model3",
+      image_trim: "_std",
+      specs: {
+        Battery: "Standard Range",
+        Acceleration: "5.3s 0-60 mph",
+        Range: "263 miles",
+        Drive: "Rear-Wheel Drive",
+        Seating: "5 Adults",
+        Wheels: '18 inches or 19 inches',
+        Weight: "3,582 lbs",
+        Cargo: "15 cu ft",
+        Displays: '15 inches Center Touchscreen',
+        "Supercharger Max/Payment Type": "170 kW max; Pay Per Use",
+        "Onboard Charger Max": "7.6 kW max (32A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 mi, whichever comes first <br/>Battery & Drive Unit - 8 years or 100,000 mi, whichever comes first",
+      },
+      miles_range: 263,
+      topspeed_mph: 140,
+      zero_to_sixty: 5.3,
+      wheel: {
+        '18 inches Aero Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_18",
+        },
+        '19 inches Sport Wheels': { range: null, price: 1500, image_wheel: "_19" },
+      },
+      interior: {
+        "All Black": { price: "included", image: "black_interior" },
+        "Black and White": { price: 1000, image: "white_interior" },
+      },
+      layout: "Five Seat Interior",
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+
+    off_menu: {
+      purchase_price: 35000,
+      image_vehicle: "model3",
+      image_trim: "_std",
+      specs: {
+        Battery: "Standard Range (Off Menu)",
+        Acceleration: "5.3s 0-60 mph",
+        Range: "220 miles",
+        Drive: "Rear-Wheel Drive",
+        Seating: "5 Adults",
+        Wheels: '18 inches or 19 inches',
+        Weight: "3,582 lbs",
+        Cargo: "15 cu ft",
+        Displays: '15 inches Center Touchscreen',
+        "Supercharger Max/Payment Type": "170 kW max; Pay Per Use",
+        "Onoard Charger Max": "7.6 kW max (32A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 mi, whichever comes first <br/>Battery & Drive Unit - 8 years or 100,000 mi, whichever comes first",
+      },
+      miles_range: 220,
+      topspeed_mph: 140,
+      zero_to_sixty: 5.3,
+      wheel: {
+        '18 inches Aero Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_18",
+        },
+        '19 inches Sport Wheels': { range: null, price: 1500, image_wheel: "_19" },
+      },
+      interior: {
+        "All Black": { price: "included", image: "black_interior" },
+        "Black and White": { price: 1000, image: "white_interior" },
+      },
+      layout: "Five Seat Interior",
+      autopilot: {
+        "No autopilot": null,
+        autopilot: { price: 3000 },
+        FSD: { price: 10000 },
+      },
+      tow_hitch: null,
+    },
+
+    long_range: {
+      purchase_price: 46990,
+      image_vehicle: "model3",
+      image_trim: "_std",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "4.2s 0-60 mph",
+        Range: "353 miles",
+        Drive: "Dual Motor All-Wheel Drive",
+        Seating: "5 Adults",
+        Wheels: '18 inches or 19 inches',
+        Weight: "4,065 lbs",
+        Cargo: "15 cu ft",
+        Displays: '15 inches Center Touchscreen',
+        "Supercharger Max/Payment Type": "250 kW max; Pay Per Use",
+        "Onboard Charger Max": "11.5 kW max (48A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 mi, whichever comes first <br/> Battery & Drive Unit - 8 years or 120,000 mi, whichever comes first",
+      },
+      miles_range: 263,
+      topspeed_mph: 140,
+      zero_to_sixty: 5.3,
+      wheel: {
+        '18 inches Aero Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_18",
+        },
+        '19 inches Sport Wheels': { range: null, price: 1500, image_wheel: "_19" },
+      },
+      interior: {
+        "All Black": { price: "included", image: "black_interior" },
+        "Black and White": { price: 1000, image: "white_interior" },
+      },
+      layout: "Five Seat Interior",
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+
+    performance: {
+      purchase_price: 54990,
+      image_vehicle: "model3",
+      image_trim: "_perf",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "3.1s 0-60 mph",
+        Range: "315 miles",
+        Drive: "Dual Motor All-Wheel Drive",
+        Seating: "5 Adults",
+        Wheels: '20 inches',
+        Weight: "4,065 lbs",
+        Cargo: "15 cu ft",
+        Displays: '15 inches Center Touchscreen',
+        "Supercharger Max/Payment Type": "250 kW max; Pay Per Use",
+        "Onboard Charger Max": "11.5 kW max (48A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 mi, whichever comes first <br/> Battery & Drive Unit - 8 years or 120,000 mi, whichever comes first",
+      },
+      miles_range: 315,
+      topspeed_mph: 162,
+      zero_to_sixty: 3.1,
+      wheel: {
+        '20 inches Überturbine Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_20",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "black_interior" },
+        "Black and White": { price: 1000, image: "white_interior" },
+      },
+      layout: "Five Seat Interior",
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+
+    plaid: null,
+  },
+
+  modelS: {
+    partial_premium_interior: null,
+    premium_interior:
+      "Premium audio system specifically tuned for a Tesla’s ultra-quiet cabin <br/> Cold weather features including heated seats for every passenger, heated steering wheel, wiper blade defrosters and washer nozzle heaters <br/> HEPA air filtration system prevents viruses, bacteria and offensive odors from entering the cabin <br/> Premium Connectivity (1 year included) <br/> Music and media over Bluetooth® <br/> Location-aware automatic garage door opener <br/> LED fog lamps <br/> Tinted glass roof with ultraviolet and infrared protection <br/> Auto dimming, power folding, heated side mirrors <br/> Custom driver profiles <br/> Wireless phone charging in center console",
+
+    model: "Model S",
+    standard: null,
+    off_menu: null,
+
+    long_range: {
+      purchase_price: 69420,
+      image_vehicle: "modelS",
+      image_trim: "_lr",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "3.7 seconds 0-60 mph",
+        Range: "402 miles (EPA est.)",
+        Drive: "All-Wheel Drive",
+        Seating: "5 Adults",
+        Wheels: "19” or 21”",
+        Weight: "4,815 lbs",
+        Cargo: "28 cu ft",
+        Displays: 'Driver Display + 17" Touchscreen',
+        "Supercharger Max/Payment Type": "250 kW max; Pay Per Use",
+        "Onboard Charger Max": "11.5 kW max (48A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 miles, whichever comes first <br/> Battery & Drive Unit - 8 years or 150,000 miles, whichever comes first",
+      },
+      miles_range: 402,
+      topspeed_mph: 155,
+      zero_to_sixty: 3.7,
+      wheel: {
+        '19 inches Tempest Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_19",
+        },
+        '21 inches Sonic Carbon Twin Turbine Wheels': {
+          range: null,
+          price: 4500,
+          image_wheel: "_21",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "ui_bundle_black" },
+        "Black and White": { price: 1500, image: "ui_bundle_white" },
+        Cream: { price: 1500, image: "ui_bundle_cream" },
+      },
+      layout: "Five Seat Interior",
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+
+    performance: {
+      purchase_price: 91990,
+      image_vehicle: "modelS",
+      image_trim: "_perf",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "2.3 seconds 0-60 mph",
+        Range: "387 miles (EPA est.)",
+        Drive: "All-Wheel Drive",
+        Seating: "5 Adults",
+        Wheels: "19” or 21”",
+        Weight: "4,929 lbs",
+        Cargo: "28 cu ft",
+        Displays: 'Driver Display + 17" Touchscreen',
+        "Supercharger Max/Payment Type": "250 kW max; Pay Per Use",
+        "Onboard Charger Max": "11.5 kW max (48A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 miles, whichever comes first <br/> Battery & Drive Unit - 8 years or 150,000 miles, whichever comes first",
+      },
+      miles_range: 334,
+      topspeed_mph: 163,
+      zero_to_sixty: 2.3,
+      wheel: {
+        '19 inches Tempest Wheels': {
+          range: "Est. Range 387mi",
+          price: "included",
+          image_wheel: "_19",
+        },
+        '21 inches Sonic Carbon Twin Turbine Wheels': {
+          range: "Est. Range 334mi",
+          price: 4500,
+          image_wheel: "_21",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "ui_bundle_black_cf" },
+        "Black and White": { price: 1500, image: "ui_bundle_white_cf" },
+        Cream: { price: 1500, image: "ui_bundle_cream" },
+      },
+      layout: "Five Seat Interior",
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+
+    plaid: {
+      purchase_price: 139990,
+      image_vehicle: "modelS",
+      image_trim: "_perf",
+      specs: null,
+      miles_range: "520+",
+      topspeed_mph: 200,
+      zero_to_sixty: "<2.0",
+      wheel: {
+        '19 inches Tempest Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_19",
+        },
+        '21 inches Sonic Carbon Twin Turbine Wheels': {
+          range: null,
+          price: 4500,
+          image_wheel: "_21",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "ui_bundle_black_cf" },
+        "Black and White": { price: 1500, image: "ui_bundle_white_cf" },
+        Cream: { price: 1500, image: "ui_bundle_cream" },
+      },
+      layout: "Five Seat Interior",
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+  },
+
+  modelX: {
+    partial_premium_interior: null,
+    premium_interior:
+      "Premium audio system specifically tuned for a Tesla’s ultra-quiet cabin <br/> Cold weather features including heated seats for every passenger, heated steering wheel, wiper blade defrosters and washer nozzle heaters <br/> HEPA air filtration system prevents viruses, bacteria and offensive odors from entering the cabin <br/> Premium Connectivity (1 year included) <br/> Music and media over Bluetooth® <br/> Location-aware automatic garage door opener <br/> LED fog lamps <br/> Tinted glass roof with ultraviolet and infrared protection <br/> Auto dimming, power folding, heated side mirrors <br/> Custom driver profiles <br/> Wireless phone charging in center console",
+
+    model: "Model X",
+    standard: null,
+    off_menu: null,
+
+    long_range: {
+      purchase_price: 79990,
+      image_vehicle: "modelX",
+      image_trim: "_lr",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "4.4s 0-60 mph",
+        Range: "371 miles (EPA est.)",
+        Drive: "All-Wheel Drive",
+        Seating: "Up to 7 Adults",
+        Wheels: "20” or 22”",
+        Weight: "5,437 lbs",
+        Cargo: "88 cu ft",
+        Displays: 'Driver Display + 17" Touchscreen',
+        "Supercharger Max/Payment Type": "250 kW max; Pay Per Use",
+        "Onboard Charger Max": "11.5 kW max (48A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 miles, whichever comes first <br/> Battery & Drive Unit - 8 years or 150,000 miles, whichever comes first",
+      },
+      miles_range: 371,
+      topspeed_mph: 155,
+      zero_to_sixty: 4.4,
+      wheel: {
+        '20 inches Silver Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_20",
+        },
+        '22 inches Onyx Black Wheels': {
+          range: null,
+          price: 5500,
+          image_wheel: "_22",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "ui_bundle_black_cf" },
+        "Black and White": { price: 1500, image: "ui_bundle_white_cf" },
+        Cream: { price: 1500, image: "ui_bundle_cream" },
+      },
+      layout: {
+        "5 Seater": { price: "included" },
+        "6 Seater": { price: 6500 },
+        "7 Seater": { price: 3500 },
+      },
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+
+    performance: {
+      purchase_price: 99990,
+      image_vehicle: "modelX",
+      image_trim: "_perf",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "2.6s 0-60 mph",
+        Range: "341 miles (EPA est.)",
+        Drive: "All-Wheel Drive",
+        Seating: "Up to 7 Adults",
+        Wheels: "20” or 22”",
+        Weight: "5,498 lbs",
+        Cargo: "88 cu ft",
+        Displays: 'Driver Display + 17" Touchscreen',
+        "Supercharger Max/Payment Type": "250 kW max; Pay Per Use",
+        "Onboard Charger Max": "11.5 kW max (48A)",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 miles, whichever comes first <br/> Battery & Drive Unit - 8 years or 150,000 miles, whichever comes first",
+      },
+      miles_range: 341,
+      topspeed_mph: 163,
+      zero_to_sixty: 2.6,
+      wheel: {
+        '20 inches Silver Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_20",
+        },
+        '22 inches Onyx Black Wheels': {
+          range: null,
+          price: 5500,
+          image_wheel: "_22",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "ui_bundle_black_cf" },
+        "Black and White": { price: 1500, image: "ui_bundle_white_cf" },
+        Cream: { price: 1500, image: "ui_bundle_cream" },
+      },
+      layout: {
+        "5 Seater": { price: "included" },
+        "6 Seater": { price: 6500 },
+        "7 Seater": { price: 3500 },
+      },
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: null,
+    },
+
+    plaid: null,
+  },
+
+  modelY: {
+    partial_premium_interior: null,
+    premium_interior:
+      "12-way power adjustable front and rear heated seats <br/> 2nd row seats that fold flat <br/> Premium audio – 14 speakers, 1 subwoofer, 2 amps, and immersive sound <br/> LED fog lamps <br/> Tinted glass roof with ultraviolet and infrared protection <br/> Power folding, heated side mirrors <br/> Music and media over Bluetooth® <br/> Custom driver profiles <br/> Center console with storage, 4 USB ports and docking for 2 smartphones",
+
+    model: "Model Y",
+    standard: null,
+    off_menu: null,
+
+    long_range: {
+      purchase_price: 49990,
+      image_vehicle: "modelY",
+      image_trim: "_lr",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "4.8s 0-60 mph",
+        Range: "326 miles (EPA est.)",
+        Drive: "Dual Motor All-Wheel Drive",
+        Seating: "Up to 7 Adults",
+        Wheels: '19 inches or 20"',
+        Weight: "4,416 lbs",
+        "Max Cargo Volume": "68 cu ft",
+        "Top Speed": "135 mph",
+        Displays: '15 inches Center Touchscreen',
+        Supercharging: "Pay Per Use",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 mi, whichever comes first <br/> Battery & Drive Unit - 8 years or 120,000 mi, whichever comes first",
+      },
+      miles_range: 326,
+      topspeed_mph: 135,
+      zero_to_sixty: 4.8,
+      wheel: {
+        '19 inches Gemini Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_19",
+        },
+        '20 inches Onyx Black Wheels': {
+          range: null,
+          price: 2000,
+          image_wheel: "_20",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "black_interior" },
+        "Black and White": { price: 1000, image: "white_interior" },
+      },
+      layout: { "5 Seater": { price: "included" } },
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: { price: 1000 },
+    },
+
+    performance: {
+      purchase_price: 59990,
+      image_vehicle: "modelY",
+      image_trim: "_perf",
+      specs: {
+        Battery: "Long Range",
+        Acceleration: "3.5s 0-60 mph",
+        Range: "303 miles (EPA est.)",
+        Drive: "Dual Motor All-Wheel Drive",
+        Seating: "Up to 7 Adults",
+        Wheels: '21 inches',
+        Weight: "4,416 lbs",
+        "Max Cargo Volume": "68 cu ft",
+        "Top Speed": "155 mph",
+        Displays: '15 inches Center Touchscreen',
+        Supercharging: "Pay Per Use",
+        Warranty:
+          "Basic Vehicle - 4 years or 50,000 mi, whichever comes first <br/> Battery & Drive Unit - 8 years or 120,000 mi, whichever comes first",
+      },
+      miles_range: 303,
+      topspeed_mph: 155,
+      zero_to_sixty: 3.5,
+      wheel: {
+        '21 inches Überturbine Wheels': {
+          range: null,
+          price: "included",
+          image_wheel: "_21",
+        },
+      },
+      interior: {
+        "All Black": { price: "included", image: "black_interior" },
+        "Black and White": { price: 1000, image: "white_interior" },
+      },
+      layout: { "5 Seater": { price: "included" } },
+      autopilot: { autopilot: { price: "included" }, FSD: { price: 10000 } },
+      tow_hitch: { price: 1000 },
+    },
+
+    plaid: null,
+  },
+};
