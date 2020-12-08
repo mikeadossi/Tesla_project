d65db746a8b982e19474f31c47c243370453de82
Mike Adossi <mikeadossi@gmail.com>
2020-12-08 12:21:10 -0600

    fix removeModel function

--- client/src/containers/VehiclePanel/VehiclePanel.js
+++ client/src/containers/VehiclePanel/VehiclePanel.js
@@ -1,35 +1,31 @@
-import React, { Component, useState } from "react"; 
+import React, { Component, useState } from "react";
+import { createStore } from 'redux'; 
 import "./VehiclePanel.css";
 import VehicleMenu from "../../components/VehicleData/VehicleMenu/VehicleMenu";
 import VehicleConfig from "../../components/VehicleData/VehicleConfig/VehicleConfig";
 
 const VehiclePanel = () => {
-  const [vehicleData, setVehicleData] = useState([]);
+  let [vehicleData, setVehicleData] = useState([]);
   const [visibility, setVisibility] = useState(false);
   const [vehicleContent, setVehicleContent] = useState({});
 
   const vehicleChoice = (model) => {
-    setVisibility(true);
-    const thisData = [{ name: model }, ...vehicleData];
-    setVehicleData(thisData);
-    // setVehicleData([{ name: model }, ...vehicleData]);
+    setVisibility(true); 
+    setVehicleData([{ name: model }, ...vehicleData]);
   };
 
+
   const removeModel = (model) => {
-    const stateData = vehicleData;
-    console.log('model: ',model)
-    console.log('stateData 1 ---> ',stateData)
-    for(let i = 0; i < stateData.length; i++){
-      console.log('i: ',i);
-      console.log('i.name: ',stateData[i].name);
-      if(stateData[i].name === model){
-        stateData.splice(0,1);
+    const stateData = vehicleData; 
+    for(let i = 0; i < stateData.length; i++){ 
+      if(stateData[i].name === model && i > -1){
+        stateData.splice(i,1);
       }
     }
 
-    setVehicleData(stateData);
-    console.log('stateData 2 ---> ',stateData)
+    setVehicleData([...stateData]); 
   }
+    
 
   const getVehicleData = async (model) => {
     const getdata = await fetch(`http://localhost:3002/model?model=${model}`);
