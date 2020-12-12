60438aa60670aeb1bed9d72f4a6c3e873574d31a
Mike Adossi <mikeadossi@gmail.com>
2020-12-11 23:22:18 -0600

    create new route allModels

--- server.js
+++ server.js
@@ -1,38 +1,64 @@
 const queries = require("./queries");
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const PORT = process.env.PORT || 3002; 
-require('dotenv').config({ path: __dirname + '/.env' });
-let mysql = require('mysql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 3002;
 
+require("dotenv").config({ path: __dirname + "/.env" });
+let mysql = require("mysql");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/statedata', async (req, res) => {
-    console.log('request received');
-    try {
-        const rows = await queries.getAll();
-        return res.status(200).send(JSON.stringify(rows));
-    } catch (e) {
-        return res.status(500);
-    }
+app.post("/statedata", async (req, res) => {
+  console.log("request received");
+  try {
+    const rows = await queries.getAll();
+    return res.status(200).send(JSON.stringify(rows));
+  } catch (e) {
+    return res.status(500);
+  }
 });
 
-app.get('/zipcode',  async (req, res) => {
-    const { query: { zipcode } } = req;
-    console.log('request received for zipcode: ', zipcode);
-    try {
-        const rows = await queries.getZipcodeData(zipcode);
-        return res.status(200).send(JSON.stringify(rows));
-    } catch (e) {
-        return res.status(500);
-    }
-})
+app.get("/zipcode", async (req, res) => {
+  const {
+    query: { zipcode },
+  } = req;
+  console.log("request received for zipcode: ", zipcode);
+  try {
+    const rows = await queries.getZipcodeData(zipcode);
+    return res.status(200).send(JSON.stringify(rows));
+  } catch (e) {
+    return res.status(500);
+  }
+});
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
-    // queries.initializeDatabase();
-})
+app.get("/model", async (req, res) => {
+  const {
+    query: { model },
+  } = req;
+  console.log("request received for model: ", model);
+  try {
+    const rows = await queries.getVehicleData(model);
+    return res.status(200).send(JSON.stringify(rows)); 
+  } catch (e) {
+    return res.status(500);
+  }
+});
+
+app.get("/allModels", async (req, res) => { 
+  const {} = req;
+  try {
+    const rows = await queries.getAllVehicleData();
+    console.log(rows)
+    return res.status(200).send(JSON.stringify(rows)); 
+  } catch (e) {
+    return res.status(500);
+  }
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+  // queries.seedVehiclesDatabase();
+});
