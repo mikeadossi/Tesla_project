dc933a583812b2577b339a0fa16e9d6f7271bcf5
Mike Adossi <mikeadossi@gmail.com>
2020-12-12 23:57:54 -0600

    store vehicle redux data into vehiclePanel state

--- client/src/containers/VehiclePanel/VehiclePanel.js
+++ client/src/containers/VehiclePanel/VehiclePanel.js
@@ -1,48 +1,55 @@
-import React, { Component, useState } from "react";
-import { createStore } from 'redux'; 
+import React, { Component, useState, useEffect } from "react";
+import { createStore } from "redux";
 import "./VehiclePanel.css";
 import VehicleMenu from "../../components/VehicleData/VehicleMenu/VehicleMenu";
 import VehicleConfig from "../../components/VehicleData/VehicleConfig/VehicleConfig";
-import {connect} from "react-redux";
-import {getAllVehicles} from "../../config/actions/vehicleActions";
+import { connect } from "react-redux";
+import { getAllVehicles } from "../../config/actions/vehicleActions";
 
 const VehiclePanel = (props) => {
   let [vehicleData, setVehicleData] = useState([]);
   const [visibility, setVisibility] = useState(false);
   const [vehicleContent, setVehicleContent] = useState({});
+  const [teslaModels, setTeslaModels] = useState({});
+  const [loadData, setLoadData] = useState(true);
 
   const vehicleChoice = (model) => {
-    setVisibility(true); 
-    setVehicleData([{ name: model }, ...vehicleData]);
+    setVisibility(true);
+    // setVehicleData([{ name: model }, ...vehicleData]);
   };
 
+  useEffect(() => {
+    props.getAllVehicles();
+    getTeslaData();
+  }, []);
 
   const removeModel = (model) => {
-    const stateData = vehicleData; 
-    for(let i = 0; i < stateData.length; i++){ 
-      if(stateData[i].name === model && i > -1){
-        stateData.splice(i,1);
+    const stateData = vehicleData;
+    for (let i = 0; i < stateData.length; i++) {
+      if (stateData[i].name === model && i > -1) {
+        stateData.splice(i, 1);
       }
     }
 
-    setVehicleData([...stateData]); 
-  }
-    
+    setVehicleData([...stateData]);
+  };
 
-  //const getVehicleData = async (model) => {
-  //  const getdata = await fetch(`http://localhost:3002/model?model=${model}`);
-   // let data = await getdata.json(); 
-   // return data;
-  //};
+  const getTeslaData = () => {
+    const vehicle = props.vehicle;
+    const vehicleObj = {};
 
+    if (vehicle.length > 0) {
+      for (var i = 0; i < vehicle.length; i++) {
+        vehicleObj[vehicle[i].model] = vehicle[i];
+      }
+    }
+
+    setTeslaModels(vehicleObj);
+  };
 
   const getVehicleData = async (model) => {
-    props.getAllVehicles(model)
-    console.log('props --->')
-    console.log(props)
-    return props.vehicle
-  }
-
+    // TODO write code to get model data from state
+  };
 
   return (
     <div className="app_Panel_container">
@@ -50,22 +57,28 @@ const VehiclePanel = (props) => {
         vehicleData={vehicleData}
         vehicleChoice={vehicleChoice}
         getVehicleData={getVehicleData}
-        setVehicleContent={setVehicleContent} 
+        setVehicleContent={setVehicleContent}
         setVehicleData={setVehicleData}
       />
 
       {vehicleData.map((ele) => (
-        <VehicleConfig removeModel={removeModel} vehicleContent={vehicleContent} selectedVehicle={ele} />
+        <VehicleConfig
+          removeModel={removeModel}
+          vehicleContent={vehicleContent}
+          selectedVehicle={ele}
+        />
       ))}
     </div>
   );
 };
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
+  console.log({ state });
+
   return {
     error: state.vehiclesReducer.error,
-    vehicle: state.vehiclesReducer.vehicle
-  }
+    vehicle: state.vehiclesReducer.vehicle,
+  };
 }
 
-export default connect(mapStateToProps, {getAllVehicles})(VehiclePanel);
+export default connect(mapStateToProps, { getAllVehicles })(VehiclePanel);
