70428b1db641bbd94d210c5afdfb4d6f8e48b189
Mike Adossi <mikeadossi@gmail.com>
2020-12-15 22:40:32 -0600

    write HeaderMobileMenu

---
+++ client/src/components/Header/HeaderMobileMenu/HeaderMobileMenu.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './HeaderMobileMenu.css';
+import { Link }  from 'react-router-dom';
+
+const HeaderMobileMenu  = ({username}) => { 
+  return (
+    <div className="headerMobileMenu app_marginTop">
+        <div className="headerMobileMenu_contents">
+            <div className="">Welcome {username}</div>
+            <div className="headerMobileMenu_close">X</div>
+            <Link to="/" className="headerMobileMenu_links app_textdecorationNone">
+              <div className="headerMobileMenu_about headerMobileMenu_login_btn">About</div>
+            </Link>
+            <Link to="/userSignup" className="headerMobileMenu_links app_textdecorationNone">
+              <div className="headerMobileMenu_sign_up headerMobileMenu_login_btn">Sign up</div>
+            </Link>
+            <Link to="/userLogin" className="headerMobileMenu_links app_textdecorationNone">
+              <div className="headerMobileMenu_log_in headerMobileMenu_login_btn">Log in</div>
+            </Link> 
+            <Link to="/userLogin" className="headerMobileMenu_links app_textdecorationNone">
+              <div className="headerMobileMenu_log_in headerMobileMenu_login_btn">Log out</div> 
+            </Link> 
+        </div>
+    </div>
+  ); 
+}
+
+export default HeaderMobileMenu;
