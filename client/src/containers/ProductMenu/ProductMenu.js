e53ee9ac71c24873a6551939ea170a1249fa4fd1
Mike Adossi <mikeadossi@gmail.com>
2020-12-29 13:25:18 -0600

    update ProductMenu css styling

--- client/src/containers/ProductMenu/ProductMenu.js
+++ client/src/containers/ProductMenu/ProductMenu.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './ProductMenu.css';
-import { Link }  from 'react-router-dom';
+import { Link }  from 'react-router-dom'; 
 
-class ProductMenu extends Component {
-  render() {
-    return (
-      <div>
-        <div className="productMenu_container">
-          <div className="productMenu_subcontainer app_displayFlex">
-            <div className="productMenu_hamburger_container">
-              <img className="productMenu_hamburger_btn" src="../../../../images/hamburger_grey.png" />
-            </div> 
-            <div className="productMenu_btn_container">
-              <Link to="/vehicles" className="">
-                <h3 className="productMenu_btn vehicle_menu_btn">Vehicle</h3>
-              </Link>
-              <Link to="/solar" className="">
-                <h3 className="productMenu_btn solar_menu_btn">Solar</h3>
-              </Link>
-            </div>
+const ProductMenu = () => {
+
+  return (
+    <div>
+      <div className="productMenu_container">
+        <div className="productMenu_subcontainer app_displayFlex">
+          <div className="productMenu_hamburger_container">
+            <img className="productMenu_hamburger_btn" src="../../../../images/Nav/hamburger_grey.png" />
+          </div> 
+          <div className="productMenu_btn_container">
+            <Link to="/vehicles" className="">
+              <div className="productMenu_btn vehicle_menu_btn">VEHICLE</div>
+            </Link>
+            <Link to="/solar" className="">
+              <div className="productMenu_btn solar_menu_btn">SOLAR</div>
+            </Link>
           </div>
         </div>
-        <div className="productMenu_loggedIn_container">
-         <div className="productMenu_loggedIn">You are logged in!</div>
-        </div>
       </div>
-    );
-  }
+      <div className="productMenu_loggedIn_container">
+        <div className="productMenu_loggedIn">You are logged in!</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductMenu;
