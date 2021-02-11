8c5a1f8bf6f9852a18344e969b27921af0dd1fdc
Mike Adossi <mikeadossi@gmail.com>
2021-02-11 00:03:10 -0800

    edit Header Mobile Menu styling

--- client/src/components/Header/HeaderMobileMenu/HeaderMobileMenu.js
+++ client/src/components/Header/HeaderMobileMenu/HeaderMobileMenu.js
@@ -5,20 +5,36 @@ import { Link }  from 'react-router-dom';
 const HeaderMobileMenu  = () => { 
   return (
     <div className="headerMobileMenu app_marginTop">
-        <div className="headerMobileMenu_contents">
-            <div className="">Welcome username</div>
-            <div className="headerMobileMenu_close">X</div>
+        <div className="headerMobileMenu_contents"> 
+            <div
+              className="vehicleConfig_close_container" 
+            >
+              <img
+                  className="vehicleConfig_close"
+                  src="../../../../images/Nav/close_2.png"
+                  alt="close"
+              ></img>
+            </div>
             <Link to="/" className="headerMobileMenu_links app_textdecorationNone">
-              <div className="headerMobileMenu_about headerMobileMenu_login_btn">About</div>
+              <div className="headerMobileMenu_about headerMobileMenu_login_btn">SETTINGS</div>
+            </Link>
+            <Link to="/" className="headerMobileMenu_links app_textdecorationNone">
+              <div className="headerMobileMenu_about headerMobileMenu_login_btn">ABOUT</div>
+            </Link> 
+            <Link to="/" className="headerMobileMenu_links app_textdecorationNone">
+              <div className="headerMobileMenu_about headerMobileMenu_login_btn">VEHICLE</div>
+            </Link>
+            <Link to="/" className="headerMobileMenu_links app_textdecorationNone">
+              <div className="headerMobileMenu_about headerMobileMenu_login_btn">SOLAR</div>
             </Link>
             <Link to="/userSignup" className="headerMobileMenu_links app_textdecorationNone">
-              <div className="headerMobileMenu_sign_up headerMobileMenu_login_btn">Sign up</div>
+              <div className="headerMobileMenu_sign_up headerMobileMenu_login_btn">SIGN UP</div>
             </Link>
             <Link to="/userLogin" className="headerMobileMenu_links app_textdecorationNone">
-              <div className="headerMobileMenu_log_in headerMobileMenu_login_btn">Log in</div>
+              <div className="headerMobileMenu_log_in headerMobileMenu_login_btn">LOG IN</div>
             </Link> 
             <Link to="/userLogin" className="headerMobileMenu_links app_textdecorationNone">
-              <div className="headerMobileMenu_log_in headerMobileMenu_login_btn">Log out</div> 
+              <div className="headerMobileMenu_log_in headerMobileMenu_login_btn">LOG OUT</div> 
             </Link> 
         </div>
     </div>
