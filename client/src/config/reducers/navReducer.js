ee89f14455be5d2542e57ea3a3190cacf7176f22
Mike Adossi <mikeadossi@gmail.com>
2021-02-17 23:53:26 -0800

    add toggle methods to redux config

--- client/src/config/reducers/navReducer.js
+++ client/src/config/reducers/navReducer.js
@@ -3,6 +3,9 @@ import * as types from "../actions/types";
 const initialState = {
   zipcode_data: {},
   error: "",
+  displayMobileMenu: false,
+  displayResetWarning: false,
+  displayApplyAllWarning: false
 };
 
 export default function (state = initialState, action) {
@@ -11,13 +14,31 @@ export default function (state = initialState, action) {
       return {
         ...state,
         zipcode_data: action.payload,
-        error: "",
+        error: ""
       };
     case types.GET_ALL_ZIPCODE_DATA_ERROR:
       return {
         ...state,
         error: action.payload,
-        zipcode_data: {},
+        zipcode_data: {}
+      };
+    case types.TOGGLE_MOBILE_MENU:
+      const { displayMobileMenu } = state;
+      return {
+        ...state,
+        displayMobileMenu: !displayMobileMenu
+      };
+    case types.TOGGLE_RESET_WARNING:
+      const { displayResetWarning } = state;
+      return {
+        ...state,
+        displayResetWarning: !displayResetWarning
+      };
+    case types.TOGGLE_APPLY_ALL_WARNING:
+      const { displayApplyAllWarning } = state;
+      return {
+        ...state,
+        displayApplyAllWarning: !displayApplyAllWarning
       };
     default:
       return state;
