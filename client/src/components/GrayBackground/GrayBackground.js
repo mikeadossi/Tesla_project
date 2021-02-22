68a99a7cf70767af8b4184b78c1daeeb929e2ca4
Mike Adossi <mikeadossi@gmail.com>
2021-02-22 00:04:30 -0800

    rewrit GrayBackground

--- client/src/components/GrayBackground/GrayBackground.js
+++ client/src/components/GrayBackground/GrayBackground.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import './GrayBackground.css';
+import React from "react";
+import "./GrayBackground.css";
 import HeaderMobileMenu from "../Header/HeaderMobileMenu/HeaderMobileMenu.js";
+import DisplayResetWarning from "../WarningModals/DisplayResetWarning/DisplayResetWarning.js";
+import DisplayApplyAllWarning from "../WarningModals/DisplayApplyAllWarning/DisplayApplyAllWarning.js";
+import { connect } from "react-redux";
+import {
+  TOGGLE_MOBILE_MENU,
+  TOGGLE_RESET_WARNING,
+  TOGGLE_APPLY_ALL_WARNING,
+} from "../../config/actions/types";
 
-const GrayBackground  = ({ menuVisibility, closeMobileMenu }) => { 
+const GrayBackground = ({
+  menuVisibility,
+  toggleMobileMenu,
+  toggleResetWarning,
+  toggleApplyAllWarning,
+  displayMobileMenu,
+  displayResetWarning,
+  displayApplyAllWarning,
+  handleSuccess
+}) => {
+  if (!displayMobileMenu && !displayResetWarning && !displayApplyAllWarning) {
+    return null;
+  }
+
+
+  const closeMobileMenu = () => toggleMobileMenu();
+  const closeResetWarning = () => toggleResetWarning();
+  const closeApplyAllWarning = () => toggleApplyAllWarning();
+
   return (
     <div className="grayBackground app_marginTop">
-      {menuVisibility.mobileMenu ? ( 
+      {displayMobileMenu && (
         <HeaderMobileMenu closeMobileMenu={closeMobileMenu} />
-        ) : (
-          ""
+      )}
+      {displayResetWarning && (
+        <DisplayResetWarning closeResetWarning={closeResetWarning} handleSuccess={handleSuccess} /> 
+      )}
+      {displayApplyAllWarning && (
+        <DisplayApplyAllWarning closeApplyAllWarning={closeApplyAllWarning} handleSuccess={handleSuccess} />
       )}
     </div>
-  ); 
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    displayMobileMenu: state.navReducer.displayMobileMenu,
+    displayResetWarning: state.navReducer.displayResetWarning,
+    displayApplyAllWarning: state.navReducer.displayApplyAllWarning,
+  };
+};
+
+function mapDispatchToProps(dispatch) {
+  return {
+    toggleMobileMenu: () => dispatch({ type: TOGGLE_MOBILE_MENU }),
+    toggleResetWarning: () => dispatch({ type: TOGGLE_RESET_WARNING }),
+    toggleApplyAllWarning: () => dispatch({ type: TOGGLE_APPLY_ALL_WARNING }),
+  };
 }
 
-export default GrayBackground;
+export default connect(mapStateToProps, mapDispatchToProps)(GrayBackground);
