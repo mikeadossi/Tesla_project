65103a5183cb724e4f5c456649d8abd7b45cbb0f
Mike Adossi <mikeadossi@gmail.com>
2021-03-05 16:32:57 -0800

    add Settings and Notifications pages

---
+++ client/src/containers/Notifications/Notifications.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Notifications.css";
+import { Link } from "react-router-dom";
+
+const Notifications = () => { 
+    return <div className="notifications_container app_pageHeight">
+        <h1 className="notifications_title">NOTIFICATIONS</h1>
+        <div className="notifications_content_container">
+            <div className="notifications_content">
+                <div className="notitifcation_date">Feb. 30, 2021</div>
+                <div className="notifications_text">listen EADDRINUSE: address already in use 0.0.0.0:3002</div>
+            </div>
+            <div className="notifications_content">
+                <div className="notitifcation_date">Feb. 30, 2021</div>
+                <div className="notifications_text">listen EADDRINUSE: address already in use 0.0.0.0:3002</div>
+            </div>
+            <div className="notifications_content">
+                <div className="notitifcation_date">Feb. 30, 2021</div>
+                <div className="notifications_text">listen EADDRINUSE: address already in use 0.0.0.0:3002</div>
+            </div>
+        </div>
+        <div className="more_notifications">
+            <div className="open_notifications">...</div>
+        </div>
+        <div className="past_notifications">
+            <div className="notifications_content">
+                <div className="notitifcation_date">Feb. 30, 2021</div>
+                <div className="notifications_text">listen EADDRINUSE: address already in use 0.0.0.0:3002</div>
+            </div>
+            <div className="notifications_content">
+                <div className="notitifcation_date">Feb. 30, 2021</div>
+                <div className="notifications_text">listen EADDRINUSE: address already in use 0.0.0.0:3002</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Notifications;
---
+++ client/src/containers/Settings/Settings.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Settings.css";
+import { Link } from "react-router-dom";
+
+const Settings = () => { 
+    return <div className="settings_container app_pageHeight">
+        <h1 className="settings_title">SETTINGS</h1> 
+        <div className="settings_signedIn">Signed in as useremail@gmail.com</div> 
+        <div className="settings_section">
+            <h3 className="">SHOW/HIDE WARNINGS</h3>
+            <div className="settings_subsection app_displayFlex">
+                <div className="settings_text">Show warning</div>
+                <button className="settings_close">Turn Off</button>
+            </div>
+            <div className="settings_subsection app_displayFlex">
+                <div className="settings_text">Show warning</div>
+                <button className="settings_close">Turn Off</button>
+            </div>
+            <div className="settings_subsection app_displayFlex">
+                <div className="settings_text">Show Notifications</div>
+                <button className="settings_close">Turn Off</button>
+            </div>
+        </div> 
+        <div className="settings_section">
+            <h3 className="">CHANGE PASSWORD</h3>
+            <div className="">
+                <div className="settings_subsection app_displayFlex">
+                    <div className="settings_text">Current Password</div>
+                    <input className="settings_password_entry" />
+                </div>
+                <div className="settings_subsection app_displayFlex">
+                    <div className="settings_text">New Password</div>
+                    <input className="settings_password_entry" />
+                </div>
+                <div className="settings_subsection app_displayFlex">
+                    <div className="settings_text">New Password</div>
+                    <input className="settings_password_entry" />
+                </div> 
+            </div>
+            <button className="settings_close">Change Password</button>
+        </div>
+        <div className="settings_section">
+            <h3 className="">CANCEL ACCOUNT</h3>
+            <div className="settings_subsection app_displayFlex">
+                <div className="settings_text">Close account</div>
+                <button className="settings_close">Close</button>
+            </div>
+        </div> 
+    </div>
+}
+
+export default Settings;
