6dcb36cc5ea873cfe5a9be17936623bf01c54b4d
Mike Adossi <mikeadossi@gmail.com>
2021-07-24 09:06:06 -0700

    modify store.js

--- client/src/store.js
+++ client/src/store.js
@@ -18,7 +18,9 @@ store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
   )
 );
 
