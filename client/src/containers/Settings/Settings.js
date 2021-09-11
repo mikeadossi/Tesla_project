3b162cfc5074d53c8c3c22e8a5e73eaefadfe332
Mike Adossi <mikeadossi@gmail.com>
2021-09-11 02:14:08 -0700

    add changePassword to Settings component

--- client/src/containers/Settings/Settings.js
+++ client/src/containers/Settings/Settings.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Settings.css";
-import { connect } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { updateUserDetails } from "../../config/actions/userActions";
+import { useHistory } from "react-router-dom"; 
+import axios from "axios";
 
-const Settings = ({ 
-    updateUserDetails,
+const Settings = ({  
     currentUser,
     setCurrentUser,
 }) => { 
+    const currentPasswRef = useRef();
+    const newPasswRef = useRef();
+    const newPasswConfirmRef = useRef();
+
+    const [settingsError, setSettingsError] = useState("");
+    const [settingsSuccessMsg, setSettingsSuccessMsg] = useState(""); 
+
     // const history = useHistory(); 
 
     // if(currentUser === null || {}){
@@ -22,31 +27,81 @@ const Settings = ({
         notifications_on: currentUser["notifications_on"],
     }) 
 
-    const handleWarning = (ObjKey) => {
+    const handleWarning = async (ObjKey) => {
         let newWarnings = {...warnings} 
         let newCurrentUser = {...currentUser};
-        if(newWarnings[ObjKey] === true || newWarnings[ObjKey] === "" || newWarnings[ObjKey] === "undefined"){ 
-            newWarnings[ObjKey] = false;
-            newCurrentUser[ObjKey] = false;
-        } else if(newWarnings[ObjKey] === false) { 
-            newWarnings[ObjKey] = true;
-            newCurrentUser[ObjKey] = true;
-        }  
+
+        if(newWarnings[ObjKey] === 'true' ){ 
+            newWarnings[ObjKey] = 'false';
+            newCurrentUser[ObjKey] = 'false';
+        } else if(newWarnings[ObjKey] === 'false') { 
+            newWarnings[ObjKey] = 'true';
+            newCurrentUser[ObjKey] = 'true';
+        }
         setWarnings(newWarnings);
         setCurrentUser(newCurrentUser);
         
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
         const parcel = {
             id: newCurrentUser.id,
             ourKey: ObjKey,
             ourValue: newCurrentUser[ObjKey],
-        } 
-        console.log('parcel: ',parcel)
-        return updateUserDetails(parcel);
+        }
+          
+        await axios.post(`http://localhost:3002/updateUserData`, parcel, axiosConfig); 
     }
 
     const closeAccount = () => {
         console.log('account closed!')
     } 
+
+    const changePassword = async (e) => {
+        e.preventDefault();
+
+        if(newPasswConfirmRef.current.value !== newPasswRef.current.value){
+            setSettingsError("Passwords do not match!");
+        } else {
+            // confirm submitted "current password" matches users account password
+            const axiosConfig = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            const body = {
+                email: currentUser.user_email,
+                password: currentPasswRef.current.value,
+            } 
+
+            const user = await axios.post(`http://localhost:3002/logUserIntoApp`, body, axiosConfig);
+            
+            if(user){ 
+                // submitted "current password" matches, now update password
+                const parcel = {
+                    id: currentUser.id,
+                    ourKey: "user_password",
+                    ourValue: newPasswRef.current.value,
+                } 
+                  
+                const success = await axios.post(`http://localhost:3002/updateUserData`, parcel, axiosConfig); 
+
+                if(success){   
+                    newPasswConfirmRef.current.value = '';
+                    newPasswRef.current.value = ''; 
+                    currentPasswRef.current.value = ''; 
+                    setSettingsSuccessMsg("Password has been changed!");
+                }
+            } else {
+                setSettingsError("Incorrect Current Password given!");
+            }
+            
+        } 
+    }
 
 
 
@@ -57,7 +112,7 @@ const Settings = ({
             <h3 className="">SHOW/HIDE WARNINGS</h3>
             <div className="settings_subsection app_displayFlex">
                 <div className="settings_text">Apply All warning</div>
-                {warnings["apply_all_warning_on"] ? (
+                {warnings["apply_all_warning_on"] === 'true' ? (
                     <button 
                         onClick={() => handleWarning("apply_all_warning_on")} 
                         className="settings_close"
@@ -75,7 +130,7 @@ const Settings = ({
             </div>
             <div className="settings_subsection app_displayFlex">
                 <div className="settings_text">Reset warning</div>
-                {warnings["reset_warning_on"] ? (
+                {warnings["reset_warning_on"] === 'true' ? (
                     <button 
                         onClick={() => handleWarning("reset_warning_on")} 
                         className="settings_close"
@@ -93,7 +148,7 @@ const Settings = ({
             </div>
             <div className="settings_subsection app_displayFlex">
                 <div className="settings_text">Notifications</div>
-                {warnings["notifications_on"] ? (
+                {warnings["notifications_on"] === 'true' ? (
                     <button 
                         onClick={() => handleWarning("notifications_on")} 
                         className="settings_close"
@@ -110,24 +165,61 @@ const Settings = ({
                 )} 
             </div>
         </div> 
-        <div className="settings_section">
+        <form className="settings_section" onSubmit={changePassword}>
             <h3 className="">CHANGE PASSWORD</h3>
             <div className="">
                 <div className="settings_subsection app_displayFlex">
-                    <div className="settings_text">Current Password</div>
-                    <input className="settings_password_entry" />
+                    <div className="settings_text">
+                        Current Password
+                    </div>
+                    <input 
+                        className="settings_password_entry" 
+                        type="password" 
+                        ref={currentPasswRef}
+                    />
                 </div>
                 <div className="settings_subsection app_displayFlex">
-                    <div className="settings_text">New Password</div>
-                    <input className="settings_password_entry" />
+                    <div className="settings_text">
+                        New Password
+                    </div>
+                    <input 
+                        className="settings_password_entry" 
+                        type="password"
+                        ref={newPasswRef}
+                    />
                 </div>
                 <div className="settings_subsection app_displayFlex">
-                    <div className="settings_text">New Password</div>
-                    <input className="settings_password_entry" />
+                    <div className="settings_text">
+                        New Password
+                    </div>
+                    <input 
+                        className="settings_password_entry" 
+                        type="password"  
+                        ref={newPasswConfirmRef} 
+                    />
                 </div> 
+                {settingsError ? (
+                    <div className="register_alert register_alert_password">
+                        {settingsError}
+                    </div>
+                ) : (
+                    ""
+                )}
+                {settingsSuccessMsg ? (
+                    <div className="register_alert_success register_alert_password">
+                        {settingsSuccessMsg}
+                    </div>
+                ) : (
+                    ""
+                )}
             </div>
-            <button className="settings_close">Change Password</button>
-        </div>
+            <button 
+                className="settings_close"
+                type="submit" 
+            >
+                Change Password
+            </button>
+        </form>
         <div className="settings_section">
             <h3 className="">CANCEL ACCOUNT</h3>
             <div className="settings_subsection app_displayFlex">
@@ -143,12 +235,4 @@ const Settings = ({
     </div>
 }
 
-// export default Settings;
-
-function mapStateToProps(state) {
-    return {
-      user_data: state.userReducer.user_data, 
-    };
-  }
-  
-  export default connect(mapStateToProps, { updateUserDetails })(Settings); 
+export default Settings;
