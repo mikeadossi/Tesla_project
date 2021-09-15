20b7d4f47c85617d67eec937cd73c076e158c0cd
Mike Adossi <mikeadossi@gmail.com>
2021-09-15 00:22:11 -0700

    notifications seed file

---
+++ seed_folder/notifications.js
@@ -0,0 +1,27 @@
+module.exports = {
+    newUserNotification1: {
+        text: "Welcome! This Notification page will alert you of updates regarding different Tesla products, relevant company news and site updates!",
+        link: null,
+        visited: false,
+    },
+    newUserNotification2: {
+        text: "Note: you can enable and disable notification alerts by visiting settings - click the top right hamburger menu.",
+        link: null,
+        visited: false,
+    },
+    "Aug. 25 2021": {
+        text: "New version of Tesla Mobile App leaked to public -",
+        link: ["electrek","https://electrek.co/2021/08/25/tesla-releases-new-mobile-app-ui-refresh-tons-features/"],
+        visited: true,
+    },
+    "Aug. 30 2021": {
+        text: "New software update to improve Model S suspension, Autopark -",
+        link: ["electrek","https://www.tesla.com/blog/model-y-achieves-5-star-overall-safety-rating-nhtsa"],
+        visited: true,
+    },
+    "Jan. 13 2021": {
+        text: "Model Y Achieves 5-Star Overall Safety Rating from NHTSA -",
+        link: ["tesla blog","https://electrek.co/2021/08/30/tesla-pushes-new-software-update-to-improve-model-s-suspension-autopark/"],
+        visited: true,
+    },
+}
