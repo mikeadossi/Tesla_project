cdb900d35010a9c16289d168cdae6d9cdbd42a07
Mike Adossi <mikeadossi@gmail.com>
2021-09-19 13:01:19 -0700

    add Vehicle and Solar Quotes

---
+++ client/src/containers/SolarProductPanel/SolarProductPanelMethods/displaySolarFacts.js
@@ -0,0 +1,25 @@
+const displaySolarFacts = () => {
+    const solarFacts = [
+        "The ideal conditions for solar panels to produce electricity are sunny and cold days, and some colder states are better for solar because of high electricity rates and state solar tax credits. ",
+        
+        "Solar increases the value of a home. In 2019, Zillow released a study showing that houses with solar panels sold for, on average, 4.1% more than similar houses without solar panels. For the median-valued house, this meant over $9,000 extra.",
+        
+        "Solar battery storage is becoming more popular. In the largest home solar survey, we found that only 11.5% of current residential solar installations included an energy storage system. However, this percentage is expected to rise quickly, as 60% of battery installations took place in the last 5 years.",
+        
+        "Solar is great for the environment but is also an excellent investment. The low cost and wide usability of solar panels will make them the go-to renewable energy source to help us reach net-zero carbon emissions by 2050.",
+        
+        "A solar energy system’s payback period can be under 5 years in a solar-friendly state. In California, the payback period on a solar installation is between 4.9 and 5.9 years. Part of the reason the payback period is so short is because California has some of the highest electricity prices compared to the rest of the country.",
+        
+        "The federal solar tax credit covers 26% of the cost if you install before 2023.",
+        
+        "The price for solar panels has dropped over 70% in the last 10 years. The price of solar will only go up as the value of the solar tax credit gets smaller. Right now, solar is the most economical it ever has been.",
+        
+        "Over 2 million solar energy systems have been installed in the U.S., which surpassed the 2 million-milestone in 2019, only 3 years after it installed 1 million systems - which took 40 years to accomplish.",
+        
+        "Producing solar power means you can avoid rising electricity prices. Grid power is getting more expensive and having a solar system will allow you to generate your own solar electricity, which, in turn, will provide you with a sort of insurance policy on any future rises in electricity prices.",
+        
+        "The first solar cell was made of selenium wafers in 1883, and in California, 19% of energy is generated from solar systems.",
+    ];
+}
+
+export default displaySolarFacts;
---
+++ client/src/containers/VehiclePanel/VehiclePanelMethods/displayVehicleFacts.js
@@ -0,0 +1,27 @@
+const displayVehicleFacts = () => {
+    const vehicleFacts = [
+        "Tesla may be one of the newer car companies in the country, but they are considered the second oldest publicly listed American Automaker (the first is Ford). This is because General Motors went bankrupt, and Chrysler is privately owned.",
+        
+        "Teslas have a hefty price tag, but many feel they pay for their worth because of the car’s composition. Not only can car buyers save on gas, but the automobile makes it so that drivers only have to replace the tires and windshield wipers routinely; nothing else needs regular replacement.",
+        
+        "In 2017, intense hurricanes rocked the nation of Puerto Rico, and many people were left without electricity. Musk heard about a children’s hospital that was without power, and within a month he delivered 700 solar panels to the hospital where 3,000 children and 35 chronically ill patients resided.",
+        
+        "Tesla has a ridiculous amount of patents. But instead of keeping their secrets to themselves, Tesla allows other companies to use their designs and principles. The hope is that other car manufacturers will expand the reach of electric vehicles and develop their own.",
+        
+        "Driving over rough terrain and potholes can be a drag. Engineers at Tesla sympathized with drivers and installed self-adjusting suspension capabilities. This means that the Model S remembers roads that drivers have traversed, and will automatically lift the car when faced with this terrain again. Like, what?",
+        
+        "Tesla cars are developed to be largely regenerative. That means, thanks to the braking mechanism and rechargeable battery, you’re unlikely to ever need to go replacing common bits and pieces for your vehicle. In fact, it’s rare you’ll need to repair anything beyond the wheels!",
+        
+        "Tesla really started coming to public attention in 2006, but they’ve actually been around since 2003. It was in ’06 when the company first revealed the Roadster, their landmark vehicle that’s continuing to turn heads.",
+        
+        "If you were using social media or following the news in February 2018, you might remember there being a few photos of an astronaut dummy ‘driving’ a Roadster around the Earth’s orbit. The photos are real – as Musk’s other firm, SpaceX, had a hand in sending ‘Starman’ into orbit. You can track Starman here!",
+        
+        "Tesla cars are noted for being some of the safest on the planet. In fact, Elon Musk has outwardly claimed that the Model 3 is THE safest car on the road. The CEO often shares safety testing data for all to see via Twitter.",
+        
+        "Tesla has an open patent policy. It’s true! Tesla has made many of its designs and patents open source, which means anyone can use them to their own ends.",
+        
+    ];
+        
+}
+
+export default displayVehicleFacts;
