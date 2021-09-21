e7be61c478cb9b310dc35eda26b156ec64de01a5
Mike Adossi <mikeadossi@gmail.com>
2021-09-20 22:48:37 -0700

    pass data to InfoModalData_locations

--- client/src/components/InfoModalData/InfoModalData_locations/InfoModalData_locations.js
+++ client/src/components/InfoModalData/InfoModalData_locations/InfoModalData_locations.js
@@ -1,119 +1,210 @@
-import React, { Component } from 'react';
-import './InfoModalData_locations.css';
+import React from "react";
+import "./InfoModalData_locations.css";
 
-class InfoModalData_locations extends Component {
-  render() {
-    return ( 
-      <div className="infoModalData_locations_container">
+const InfoModalData_locations = ({
+  allShowrooms,
+  allServiceCenters,
+  allChargingLocations,
+}) => { 
 
-        <h1>LOCATIONS NEARBY</h1>
-        <div className="app_modalData_content">
+  return (
+    <div className="infoModalData_locations_container">
+      <h1>LOCATIONS NEARBY</h1>
+      <div className="app_modalData_content">
+        {allShowrooms ? (
           <div className="infoModalData_showrooms_container">
             <h3 className="modalTitle">SHOWROOMS</h3>
             <div className="infoModalData_showroom_container">
-              <div className="infoPanel_showroom1">
-                <h4 className="infoPanel_showroom1_name">Santa Monica Place</h4>
-                <p className="infoPanel_showroom1_address">
-                    395 Santa Monica Place #120
-                    Santa Monica, CA 90401
+              {allShowrooms.sSortedArr[0] ? (
+                <div className="infoPanel_showroom1">
+                  <h4 className="infoPanel_showroom1_name">
+                    {allShowrooms.sObj[allShowrooms.sSortedArr[0]][0]}
+                  </h4>
+                  {allShowrooms.sObj[allShowrooms.sSortedArr[0]][2].map(
+                    (line) => (
+                      <p className="infoPanel_showroom1_address">{line}</p>
+                    )
+                  )}
+                  <p>
+                    <span>Store: </span>
+                    <span className="infoPanel_showroom1_phone">
+                      {allShowrooms.sObj[allShowrooms.sSortedArr[0]][4]}
+                    </span>
+                  </p>
+                  <p className="infoPanel_showroom1_openTimes">
+                    For Opening times and more:{" "}
+                    <a
+                      href={allShowrooms.sObj[allShowrooms.sSortedArr[0]][1]}
+                      target="_blank"
+                    >
+                      link
+                    </a>
+                  </p>
+                </div>
+              ) : (
+                ""
+              )}
+              {allShowrooms.sSortedArr[1] ? (
+                <div className="infoPanel_showroom1">
+                <h4 className="infoPanel_showroom1_name">
+                  {allShowrooms.sObj[allShowrooms.sSortedArr[1]][0]}
+                </h4>
+                {allShowrooms.sObj[allShowrooms.sSortedArr[1]][2].map(
+                  (line) => (
+                    <p className="infoPanel_showroom1_address">{line}</p>
+                  )
+                )}
+                <p>
+                  <span>Store: </span>
+                  <span className="infoPanel_showroom1_phone">
+                    {allShowrooms.sObj[allShowrooms.sSortedArr[1]][4]}
+                  </span>
                 </p>
-                <span>Store: </span>
-                <span className="infoPanel_showroom1_phone">(310) 395-8333</span>
                 <p className="infoPanel_showroom1_openTimes">
-                    Monday - Thursday 10:00am - 8:00pm
-                    Friday - Saturday 10:00am - 9:00pm
-                    Sunday 11:00am - 8:00pm
+                  For Opening times and more:{" "}
+                  <a
+                    href={allShowrooms.sObj[allShowrooms.sSortedArr[1]][1]}
+                    target="_blank"
+                  >
+                    link
+                  </a>
                 </p>
               </div>
-              <div className="infoPanel_showroom2">
-                <h4 className="infoPanel_showroom2_name">Los Angeles - Century City</h4>
-                <p className="infoPanel_showroom2_address">
-                    10250 Santa Monica Blvd
-                    Ste. 1340
-                    Los Angeles, CA 90067
-                </p>
-                <span>Store: </span>
-                <span className="infoPanel_showroom2_phone">(310) 553-5767</span>
-                <p className="infoPanel_showroom2_openTimes">
-                    Monday - Saturday 10:00am - 9:00pm
-                    Sunday 11:00am - 7:0pm
-                </p>
-              </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
+        ) : (
+          ""
+        )}
 
-          <div className="infoModalData_serviceCenters_container">
-            <h3 className="modalTitle">SERVICE CENTERS</h3>
-            <div className="infoModalData_showroom_container">
-              <div className="infoPanel_showroom1">
-                <h4 className="infoPanel_showroom1_name">Santa Monica Place</h4>
-                <p className="infoPanel_showroom1_address">
-                    395 Santa Monica Place #120
-                    Santa Monica, CA 90401
-                </p>
-                <span>Store: </span>
-                <span className="infoPanel_showroom1_phone">(310) 395-8333</span>
-                <p className="infoPanel_showroom1_openTimes">
-                    Monday - Thursday 10:00am - 8:00pm
-                    Friday - Saturday 10:00am - 9:00pm
-                    Sunday 11:00am - 8:00pm
-                </p>
-              </div>
-              <div className="infoPanel_showroom2">
-                <h4 className="infoPanel_showroom2_name">Los Angeles - Century City</h4>
-                <p className="infoPanel_showroom2_address">
-                    10250 Santa Monica Blvd
-                    Ste. 1340
-                    Los Angeles, CA 90067
-                </p>
-                <span>Store: </span>
-                <span className="infoPanel_showroom2_phone">(310) 553-5767</span>
-                <p className="infoPanel_showroom2_openTimes">
-                    Monday - Saturday 10:00am - 9:00pm
-                    Sunday 11:00am - 7:0pm
-                </p>
-              </div> 
-            </div>
-          </div>
+        {allServiceCenters ? (
+         <div className="infoModalData_showrooms_container">
+         <h3 className="modalTitle">SERVICE CENTERS</h3>
+         <div className="infoModalData_showroom_container">
+           {allServiceCenters.scSortedArr[0] ? (
+             <div className="infoPanel_showroom1">
+               <h4 className="infoPanel_showroom1_name">
+                 {allServiceCenters.scObj[allServiceCenters.scSortedArr[0]][0]}
+               </h4>
+               {allServiceCenters.scObj[allServiceCenters.scSortedArr[0]][2].map(
+                 (line) => (
+                   <p className="infoPanel_showroom1_address">{line}</p>
+                 )
+               )}
+               <p>
+                 <span>Store: </span>
+                 <span className="infoPanel_showroom1_phone">
+                   {allServiceCenters.scObj[allServiceCenters.scSortedArr[0]][4]}
+                 </span>
+               </p>
+               <p className="infoPanel_showroom1_openTimes">
+                 For Opening times and more:{" "}
+                 <a
+                   href={allServiceCenters.scObj[allServiceCenters.scSortedArr[0]][1]}
+                   target="_blank"
+                 >
+                   link
+                 </a>
+               </p>
+             </div>
+           ) : (
+             ""
+           )}
+           {allServiceCenters.scSortedArr[1] ? (
+             <div className="infoPanel_showroom1">
+             <h4 className="infoPanel_showroom1_name">
+               {allServiceCenters.scObj[allServiceCenters.scSortedArr[1]][0]}
+             </h4>
+             {allServiceCenters.scObj[allServiceCenters.scSortedArr[1]][2].map(
+               (line) => (
+                 <p className="infoPanel_showroom1_address">{line}</p>
+               )
+             )}
+             <p>
+               <span>Store: </span>
+               <span className="infoPanel_showroom1_phone">
+                 {allServiceCenters.scObj[allServiceCenters.scSortedArr[1]][4]}
+               </span>
+             </p>
+             <p className="infoPanel_showroom1_openTimes">
+               For Opening times and more:{" "}
+               <a
+                 href={allServiceCenters.scObj[allServiceCenters.scSortedArr[1]][1]}
+                 target="_blank"
+               >
+                 link
+               </a>
+             </p>
+           </div>
+           ) : (
+             ""
+           )}
+         </div>
+       </div>
+        ) : (
+          ""
+        )}
 
-          <div className="infoModalData_chargingLocations_container">
-            <h3 className="modalTitle">CHARGING LOCATIONS</h3>
-            <div className="infoModalData_showroom_container">
-              <div className="infoPanel_showroom1">
-                <h4 className="infoPanel_showroom1_name">Santa Monica Place</h4>
-                <p className="infoPanel_showroom1_address">
-                    395 Santa Monica Place #120
-                    Santa Monica, CA 90401
-                </p>
-                <span>Store: </span>
-                <span className="infoPanel_showroom1_phone">(310) 395-8333</span>
-                <p className="infoPanel_showroom1_openTimes">
-                    Monday - Thursday 10:00am - 8:00pm
-                    Friday - Saturday 10:00am - 9:00pm
-                    Sunday 11:00am - 8:00pm
-                </p>
-              </div>
-              <div className="infoPanel_showroom2">
-                <h4 className="infoPanel_showroom2_name">Los Angeles - Century City</h4>
-                <p className="infoPanel_showroom2_address">
-                    10250 Santa Monica Blvd
-                    Ste. 1340
-                    Los Angeles, CA 90067
-                </p>
-                <span>Store: </span>
-                <span className="infoPanel_showroom2_phone">(310) 553-5767</span>
-                <p className="infoPanel_showroom2_openTimes">
-                    Monday - Saturday 10:00am - 9:00pm
-                    Sunday 11:00am - 7:0pm
-                </p>
-              </div> 
-            </div>
-          </div>
-        </div>
-
+        {allChargingLocations ? (
+         <div className="infoModalData_showrooms_container">
+         <h3 className="modalTitle">CHARGING LOCATIONS</h3>
+         <div className="infoModalData_showroom_container">
+           {allChargingLocations.clSortedArr[0] ? (
+             <div className="infoPanel_showroom1">
+               <h4 className="infoPanel_showroom1_name">
+                 {allChargingLocations.clObj[allChargingLocations.clSortedArr[0]][0]}
+               </h4>
+               {allChargingLocations.clObj[allChargingLocations.clSortedArr[0]][2].map(
+                 (line) => (
+                   <p className="infoPanel_showroom1_address">{line}</p>
+                 )
+               )} 
+               <p className="infoPanel_showroom1_openTimes">
+                 For Opening times and more:{" "}
+                 <a
+                   href={allChargingLocations.clObj[allChargingLocations.clSortedArr[0]][1]}
+                   target="_blank"
+                 >
+                   link
+                 </a>
+               </p>
+             </div>
+           ) : (
+             ""
+           )}
+           {allChargingLocations.clSortedArr[1] ? (
+             <div className="infoPanel_showroom1">
+             <h4 className="infoPanel_showroom1_name">
+               {allChargingLocations.clObj[allChargingLocations.clSortedArr[1]][0]}
+             </h4>
+             {allChargingLocations.clObj[allChargingLocations.clSortedArr[1]][2].map(
+               (line) => (
+                 <p className="infoPanel_showroom1_address">{line}</p>
+               )
+             )} 
+             <p className="infoPanel_showroom1_openTimes">
+               For Opening times and more:{" "}
+               <a
+                 href={allChargingLocations.clObj[allChargingLocations.clSortedArr[1]][1]}
+                 target="_blank"
+               >
+                 link
+               </a>
+             </p>
+           </div>
+           ) : (
+             ""
+           )}
+         </div>
+       </div>
+        ) : (
+          ""
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default InfoModalData_locations;
