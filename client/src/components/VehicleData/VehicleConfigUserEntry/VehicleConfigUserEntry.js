00b14b3a252aa1b909cd3ade674636c92c32c19f
Mike Adossi <mikeadossi@gmail.com>
2021-10-11 03:35:11 -0700

    clean up

--- client/src/components/VehicleData/VehicleConfigUserEntry/VehicleConfigUserEntry.js
+++ client/src/components/VehicleData/VehicleConfigUserEntry/VehicleConfigUserEntry.js
@@ -134,7 +134,7 @@ const VehicleConfigUserEntry = ({
   const handlePaymentFormSubmit = async () => {
     // function below updates state with user entries
 
-    // setAlertUser([]);
+    setAlertUser([]);
 
     const configuredPrice = teslaModels.vehicle_render[vehicleName].cash_price;
 
